d78f862a2a9ff709f720a7f87bea355f20bbc310
kevin-bai <897840029@qq.com>
2018-06-12 01:03:32 +0800

    核心模块布局

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
-import {CoreModule} from './core/core.module'
+import {CoreModule} from './core/core.module';
 import {AppComponent} from './app.component';
 
 @NgModule({
