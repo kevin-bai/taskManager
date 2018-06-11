4a57e70b12766d514a16e56d1367532850adace3
kevin-bai <897840029@qq.com>
2018-06-12 00:41:49 +0800

    core module 加载逻辑

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,5 +1,5 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule, Optional, SkipSelf,} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
 @NgModule({
   imports: [
@@ -7,4 +7,10 @@ import { CommonModule } from '@angular/common';
   ],
   declarations: []
 })
-export class CoreModule { }
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parent: CoreModule) {
+    if (parent) {
+      throw new Error('coreModule existed !')
+    }
+  }
+}
