d1743e57521d0a2f49e5aa6f0b97089cc4ce570d
kevin-bai <897840029@qq.com>
2018-06-12 11:18:30 +0800

    ng-zorro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
+import {NgZorroAntdModule} from 'ng-zorro-antd';
 import {CoreModule} from './core/core.module';
 import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,9 @@ import {AppComponent} from './app.component';
   ],
   imports: [
     BrowserModule,
-    CoreModule
+    NgZorroAntdModule,
+    BrowserAnimationsModule,
+    CoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
