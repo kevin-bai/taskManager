d96f68d46fcfd7ea09cfa569c8ab831e23ba48ce
kevin-bai <897840029@qq.com>
2018-06-12 20:12:03 +0800

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-// import {NgZorroAntdModule} from 'ng-zorro-antd';
+import {NgZorroAntdModule} from 'ng-zorro-antd';
 import {CoreModule} from './core/core.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
@@ -11,7 +11,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    // NgZorroAntdModule,
+    NgZorroAntdModule,
     BrowserAnimationsModule,
     CoreModule,
   ],
